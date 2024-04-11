28562ee0afe4ad51158c4135240aa96601cf89e8
alireza samaei <alireza.samaei73@gmail.com>
2024-04-11 21:10:40 +0100

    First Publish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "../src/Scss/Main.scss";
+import Login from "./Components/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FirstPage from "./Components/FirstPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route exact path="/Dashboard" element={<FirstPage />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
